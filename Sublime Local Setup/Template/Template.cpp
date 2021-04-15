1ecc0b986498e2dc9cc3f62914db97b5de2be3e1
Abhishek Tripathi <abhishektripathi911@gmail.com>
2021-04-15 11:20:37 +0530

    Add files via upload

---
+++ Sublime Local Setup/Template/Template.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl ("\n")
+#define pi (3.141592653589)
+#define mod 1e9+7
+#define lli long long int
+#define vi vector<lli>
+#define pb push_back
+#define mp make_pair
+#define Max 100005
+#define ff first
+#define ss second
+#define all(c) cc.begin(), c.end()
+#define mini(a, b, c) min(c, min(a, b))
+#define mini2(a, b, c, d) min(d, min(c, min(a, b)))
+#define rloop(i, a, n) for(int i=n-1; i>=0; i--)
+#define loop(i, a, b) for(int i=0; i<n; i++)
+#define fastIO ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+//-------------------------------------------------------------------------------------------------//
+int32_t main()
+{
+	fastIO
+#ifdef NameForIfDef
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	freopen("error.txt", "w", stderr);
+#endif
+
+	// cout << "ths gos int output.txt" << '\n';
+	// cerr << "this goes into error.txt" << '\n';
+
+	int n;
+	cin >> n;
+	cout << n + 23 / 2 << endl;
+
+	return 0;
+}
+
+//-------------------------------------------------------------------------------------------------/
