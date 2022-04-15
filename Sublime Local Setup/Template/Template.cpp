e673b22500de9ed6f5474a9afee639874a0ed209
Abhishek Tripathi <abhishektripathi911@gmail.com>
2022-04-15 18:03:56 +0530

    Add files via upload

--- Sublime Local Setup/Template/Template.cpp
+++ Sublime Local Setup/Template/Template.cpp
@@ -1,39 +1,91 @@
-#include <bits/stdc++.h>
+// Template look-up : https://ncduy0303.github.io/Competitive-Programming/
+
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+
+// this makes code execute faster
+#pragma GCC optimize("Ofast")
+#pragma GCC target("popcnt")
+
 using namespace std;
-#define endl ("\n")
-#define pi (3.141592653589)
-#define mod 1e9+7
-#define lli long long int
-#define vi vector<lli>
+
+#define fastIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define mod 1000000007
+#define mod1 998244353
+#define INF 1e18
+#define nline "\n"
 #define pb push_back
+#define ppb pop_back
 #define mp make_pair
-#define Max 100005
 #define ff first
 #define ss second
-#define all(c) cc.begin(), c.end()
-#define mini(a, b, c) min(c, min(a, b))
-#define mini2(a, b, c, d) min(d, min(c, min(a, b)))
-#define rloop(i, a, n) for(int i=n-1; i>=0; i--)
-#define loop(i, a, b) for(int i=0; i<n; i++)
-#define fastIO ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
-//-------------------------------------------------------------------------------------------------//
-int32_t main()
-{
-	fastIO
-#ifdef NameForIfDef
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-	freopen("error.txt", "w", stderr);
+// #define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
 #endif
 
-	// cout << "ths gos int output.txt" << '\n';
-	// cerr << "this goes into error.txt" << '\n';
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
 
-	int n;
-	cin >> n;
-	cout << n + 23 / 2 << endl;
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+
+const long long inf = 1000000000000000000;
+const long double pi = 3.14159265358979323846264338;
+ll gcd(ll a, ll b) {if (!b)return a; return gcd(b, a % b);}
+#define sq(x) ((x)*(x))
+
+//info to remember
+//=> int: (—10 ^ 9 to + 10 ^ 9).
+//=> long long int: (—10 ^ 18 to + 10 ^ 18).
+
+//==================================================================================================================
+
+
+
+//==================================================================================================================
+int main() {
+
+//------------------------//
+	fastIO
+#ifndef ONLINE_JUDGE
+	freopen("C:/Users/ABHISHEK/Desktop/CP/Input-Output-Files/error.txt", "w", stderr);
+	cerr << "Time Taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+#endif
+//-----------------------//
+
+
 
 	return 0;
 }
 
-//-------------------------------------------------------------------------------------------------/
+//==================================================================================================================
